2b5331c2fc464075e27e9a96fd506ac741693a18
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-05-05 11:24:56 +0200

    K-1620 added theming and styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from 'styled-components';
 
 // Global Styles
 import './index.scss';
+import { theme } from './theme';
 
 // Features
 import Dashboard from './feature/dashboard';
 
 // ==========================================================
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <Dashboard />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
