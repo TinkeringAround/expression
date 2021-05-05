5d4b142b83075eae94d3f22f0847eb9479a58631
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-05-05 11:52:32 +0200

    K-1620 add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from 'styled-components';
 
@@ -12,9 +13,16 @@ import Dashboard from './feature/dashboard';
 
 // ==========================================================
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <Dashboard />
-  </ThemeProvider>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Redirect to="/dashboard" />
+      </Switch>
+    </ThemeProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
