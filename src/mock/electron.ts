b9483beba3dc14c704ae8d3898686d1f321bb454
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-05-13 10:55:30 +0200

    K-5485 add mock dispatch to electron mocks

--- src/mock/electron.ts
+++ src/mock/electron.ts
@@ -1,2 +1,5 @@
 export const mockElectronTrigger = (fn: (_: any, data: any) => void) =>
   jest.spyOn(window.electron, 'trigger').mockImplementation((_, data) => fn(null, data));
+
+export const mockElectronDispatch = (fn: (_: any, data: any) => void) =>
+  jest.spyOn(window.electron, 'dispatch').mockImplementation((_, data) => fn(null, data));
