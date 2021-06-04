a9efa55e80bf74287b50612559c4684a5781e2bb
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-06-04 09:15:11 +0200

    K-8134 refactor utility helper functions

--- src/util/index.ts
+++ src/util/index.ts
@@ -11,22 +11,27 @@ export type anyFunction = (...anyThing: any) => void;
  * @param {number} bytes count
  * @returns {string} the size in mb as string with 'MB' extension
  */
-export const bytesToMegaBytes = (bytes: number): string =>
-  `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+export const toMB = (bytes: number): string => `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
 
 /**
  * Converts a floating point duration value to fixed string with unit 's' seconds
  * @param {number} duration the float point duration in seconds
+ * @param {number} fraction the fraction as digit count after comma
  * @returns {string} the fixed duration as string with unit 's'
  */
-export const asSeconds = (duration: number): string => `${duration.toFixed(1)}s`;
+export const asSeconds = (duration: number, fraction: number = 0): string => {
+  let seconds = duration.toFixed(fraction);
+  if (fraction === 1 && seconds.charAt(seconds.length - 1) === '0') seconds = duration.toFixed(0);
+
+  return `${seconds}s`;
+};
 
 /**
  * Maps the Feature Path to a readable feature name
  * @param {string} path the feature path
  * @returns {string} the readable feature name
  */
-export const getFeatureNameByPath = (path: string): string => {
+export const featureToNameByPath = (path: string): string => {
   if (path === '/') return '';
   if (path === Features.DASHBOARD) return 'Kadenz';
   return `${path[1].toUpperCase()}${path.slice(2)}`;
@@ -41,5 +46,5 @@ export const getFeatureNameByPath = (path: string): string => {
  * @param {number} y2 the max value of second range
  * @returns {number} the value of first range mapped to second range
  */
-export const mapValues = (value: number, x1: number, y1: number, x2: number, y2: number) =>
+export const map = (value: number, x1: number, y1: number, x2: number, y2: number) =>
   ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
