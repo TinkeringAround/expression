8f0efb1eeab2a9c7341c4d3e20391dafcc362e2d
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-07-01 11:45:24 +0200

    K-49113 add marker and adjust area-selection test cases

--- src/feature/slicer/visualizer/area-selection/marker/index.tsx
+++ src/feature/slicer/visualizer/area-selection/marker/index.tsx
@@ -1,13 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { Transport } from 'tone';
 
-import { usePlayer } from '../../../../../store/player';
+import { useSlicer } from '../../../../../store/slicer';
 
 import { SMarker } from './styled';
 
 const Marker: FC = () => {
-  const { marker } = usePlayer();
+  const { file } = useSlicer();
 
-  return <SMarker style={{ left: `${marker}%` }} />;
+  const [position, setPosition] = useState<number>(0);
+
+  useEffect(() => {
+    let handler: NodeJS.Timeout;
+    // set repeat to keep track on progess in percent
+    Transport.on('start', () => {
+      handler = setInterval(() => {
+        setPosition(Transport.progress * 100);
+      }, 100);
+    });
+
+    Transport.on('pause', () => {
+      // cancel all scheduled repeats
+      setPosition(Transport.progress * 100);
+      handler && clearInterval(handler);
+    });
+
+    Transport.on('loopStart', () => {
+      setPosition(Transport.progress * 100);
+    });
+
+    Transport.on('loopEnd', () => {
+      setPosition(Transport.progress * 100);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (position > 100) setPosition(100);
+    else if (position < 0) setPosition(0);
+  }, [position, setPosition]);
+
+  useEffect(() => {
+    setPosition(Transport.progress * 100);
+  }, [file, setPosition]);
+
+  return <SMarker role="marker" style={{ left: `${position}%` }} />;
 };
 
 export default Marker;
---
+++ src/feature/slicer/visualizer/area-selection/marker/marker.test.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { act, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { AppMock } from '../../../../../mock/components';
+
+import { useSlicer } from '../../../../../store/slicer';
+
+import Marker from './index';
+
+import { getSlicerStoreMock } from '../../../../../mock/store';
+import Transport from '../../../../../mock/transport';
+
+describe('marker', () => {
+  const progress = 0.5;
+  const MarkerInApp = (
+    <AppMock>
+      <Marker />
+    </AppMock>
+  );
+
+  beforeEach(() => {
+    useSlicer.setState(getSlicerStoreMock());
+    Transport.progress = 0;
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test('should update position when Transport is started', () => {
+    jest.useFakeTimers();
+
+    render(MarkerInApp);
+    Transport.start();
+    Transport.progress = progress;
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    expect(screen.getByRole('marker')).toHaveStyle(`left: ${progress * 100}%`);
+  });
+
+  test('should update position and clear timeout handler when Transport is paused', async () => {
+    render(MarkerInApp);
+    Transport.start();
+
+    // check if not updated via start handler yet
+    expect(screen.getByRole('marker')).toHaveStyle(`left: 0%`);
+
+    Transport.progress = progress;
+    act(() => Transport.pause());
+
+    expect(screen.getByRole('marker')).toHaveStyle(`left: ${progress * 100}%`);
+  });
+
+  test('should update position when Transport emits loopStart', async () => {
+    render(MarkerInApp);
+
+    Transport.progress = progress;
+    act(() => Transport.triggerLoopStart());
+
+    expect(screen.getByRole('marker')).toHaveStyle(`left: ${progress * 100}%`);
+  });
+
+  test('should update position when Transport emits loopEnd', async () => {
+    render(MarkerInApp);
+
+    Transport.progress = progress;
+    act(() => Transport.triggerLoopEnd());
+
+    expect(screen.getByRole('marker')).toHaveStyle(`left: ${progress * 100}%`);
+  });
+
+  test('should cap position to 100', async () => {
+    Transport.progress = 5;
+    render(MarkerInApp);
+
+    expect(screen.getByRole('marker')).toHaveStyle(`left: 100%`);
+  });
+
+  test('should floor position to 0', async () => {
+    Transport.progress = -5;
+    render(MarkerInApp);
+
+    expect(screen.getByRole('marker')).toHaveStyle(`left: 0%`);
+  });
+});
