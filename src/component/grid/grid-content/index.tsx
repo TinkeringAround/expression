bd31fbe7ddaa83a09794a2657fccbdfa4c73e957
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-07-21 13:04:34 +0200

    K-4100 fix grid content style

--- src/component/grid/grid-content/index.tsx
+++ src/component/grid/grid-content/index.tsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 const GridContent = styled.div`
-  flex-grow: 1;
+  flex: 1;
+  height: 100%;
 
   padding: 1.5rem;
 
   background: ${({ theme: { light } }) => light};
 
+  box-sizing: border-box;
   overflow: hidden;
 `;
 
