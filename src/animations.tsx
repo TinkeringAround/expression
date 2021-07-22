f13a2cd30859b275e8881900ea6a6be6b1966db5
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-07-22 14:45:10 +0200

    K-4100 add fadeOut animation

--- src/animations.tsx
+++ src/animations.tsx
@@ -4,6 +4,12 @@ export const fadeIn = () => `
   100% {opacity: 1;}
 }`;
 
+export const fadeOut = () => `
+@keyframes fadeOut {
+  0% {opacity: 1;}
+  100% {opacity: 0;}
+}`;
+
 export const fromTop = (start: string, end: string) => `
 @keyframes fromTop {
   0% {top: ${start};}
