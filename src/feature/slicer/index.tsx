a12638df578a0b96186f98f5fbf098ce4390186a
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-07-23 09:13:03 +0200

    K-51164 add notification counter hint to notifications tab

--- src/feature/slicer/index.tsx
+++ src/feature/slicer/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 import { Grid, GridSidepane, GridContent, GridTabs } from '../../component/grid';
 import { Notifications } from '../../component/tabs';
+import { useNotification } from '../../store/notification';
 
 import DropZone from './drop-zone';
 import Info from './info';
@@ -11,28 +12,32 @@ import Export from './export';
 
 import { SSlicer } from './styled';
 
-const Slicer: FC = () => (
-  <Grid>
-    <GridSidepane>
-      <DropZone />
-    </GridSidepane>
+const Slicer: FC = () => {
+  const { notifications } = useNotification();
 
-    <GridContent>
-      <SSlicer>
-        <Info />
-        <Visualizer />
-        <Hint />
-      </SSlicer>
-    </GridContent>
+  return (
+    <Grid>
+      <GridSidepane>
+        <DropZone />
+      </GridSidepane>
 
-    <GridTabs
-      tabs={[
-        { name: 'Export', component: <Export /> },
-        { name: 'Notifications', component: <Notifications /> }
-      ]}
-      initialTab={0}
-    />
-  </Grid>
-);
+      <GridContent>
+        <SSlicer>
+          <Info />
+          <Visualizer />
+          <Hint />
+        </SSlicer>
+      </GridContent>
+
+      <GridTabs
+        tabs={[
+          { name: 'Export', component: <Export /> },
+          { name: 'Notifications', component: <Notifications />, count: notifications.length }
+        ]}
+        initialTab={0}
+      />
+    </Grid>
+  );
+};
 
 export default Slicer;
