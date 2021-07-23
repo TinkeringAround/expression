2f7d2a13a12dc4c42dd0d49eb290d39de918675e
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-07-23 10:08:10 +0200

    K-51164 update dashboard

--- src/feature/dashboard/dashboard.test.tsx
+++ src/feature/dashboard/dashboard.test.tsx
@@ -20,18 +20,43 @@ describe('Dashboard', () => {
 
     expect(screen.queryByText('Dashboard')).not.toBeInTheDocument();
     expect(screen.getByText('SLICER')).toBeInTheDocument();
+    expect(screen.getByText('PHRASER')).toBeInTheDocument();
+    expect(screen.getByText('FX')).toBeInTheDocument();
   });
 
-  test('renders enabled features as links with correct href', async () => {
+  test('renders all features regardless of enabled or disabled', () => {
     render(DashboardInApp);
 
     Object.keys(Features)
-      .splice(1, 1)
-      .forEach(route => {
-        const link = screen.getByText(route) as HTMLLinkElement;
+      .splice(1, 3)
+      .every(route => expect(screen.getByText(route)).toBeInTheDocument());
+  });
 
-        expect(link.tagName).toEqual('A');
-        expect(link.href.includes(route.toLowerCase())).toBeTruthy();
+  test('renders all enabled features with correct links', () => {
+    render(DashboardInApp);
+
+    Object.keys(Features)
+      .splice(1, 1) // slicer
+      .forEach(route => {
+        const heading = screen.getByText(route) as HTMLHeadingElement;
+        const link = heading.parentElement as HTMLLinkElement;
+
+        expect(link.href).toContain(Features.SLICER);
+        expect(link.classList).not.toContain('disabled');
+      });
+  });
+
+  test('renders all disabled features with link to Dashboard', () => {
+    render(DashboardInApp);
+
+    Object.keys(Features)
+      .splice(2, 3) // phraser & fx
+      .forEach(route => {
+        const heading = screen.getByText(route) as HTMLHeadingElement;
+        const link = heading.parentElement as HTMLLinkElement;
+
+        expect(link.href).toContain(Features.DASHBOARD);
+        expect(link.classList).toContain('disabled');
       });
   });
 });
--- src/features.ts
+++ src/features.ts
@@ -1,6 +1,12 @@
 export enum Features {
   DASHBOARD = '/dashboard',
   SLICER = '/slicer',
-  FX = '/fx',
-  PHRASER = '/phraser'
+  PHRASER = '/phraser',
+  FX = '/fx'
 }
+
+export const FeatureDescription: { [feature: string]: string } = {
+  SLICER: 'Listen to Audio Files, Crop them to Slices and Export them easily.',
+  PHRASER: 'Coming soon...',
+  FX: 'Coming soon...'
+};
