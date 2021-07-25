c1a39d01f44a43ff994943d5dab4039bebde625f
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-07-25 12:55:02 +0200

    K-50125 make grid-sidepane collapsable

--- src/component/grid/grid-sidepane/index.tsx
+++ src/component/grid/grid-sidepane/index.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
+
+import Icon, { IconType } from '../../icon';
 
 import { SGridSidepane } from './styled';
 
 interface Props {
-  side?: 'left' | 'right';
+  collapseWidth?: string;
   minWidth?: number;
   maxWidth?: number;
   defaultSize?: {
@@ -13,25 +15,36 @@ interface Props {
 }
 
 const GridSidepane: FC<Props> = ({
-  side = 'left',
   children,
   minWidth = 225,
   maxWidth = 400,
   defaultSize = {
     width: 300,
     height: '100%'
-  }
-}) => (
-  <SGridSidepane
-    defaultSize={defaultSize}
-    minWidth={minWidth}
-    maxWidth={maxWidth}
-    minHeight="100%"
-    maxHeight="100%"
-    enable={{ right: side === 'left', left: side === 'right' }}
-  >
-    {children}
-  </SGridSidepane>
-);
+  },
+  collapseWidth = '2rem'
+}) => {
+  const [expanded, setExpanded] = useState<boolean>(true);
+
+  const toggle = useCallback(() => {
+    setExpanded(!expanded);
+  }, [expanded, setExpanded]);
+
+  const icon: IconType = expanded ? 'arrow-double-left' : 'arrow-double-right';
+
+  return (
+    <SGridSidepane
+      defaultSize={defaultSize}
+      minWidth={expanded ? minWidth : collapseWidth}
+      maxWidth={expanded ? maxWidth : collapseWidth}
+      minHeight="100%"
+      maxHeight="100%"
+      enable={expanded ? { right: true } : undefined}
+    >
+      <Icon iconType={icon} onClick={toggle} />
+      {expanded && children}
+    </SGridSidepane>
+  );
+};
 
 export default GridSidepane;
--- src/component/grid/grid-sidepane/styled.tsx
+++ src/component/grid/grid-sidepane/styled.tsx
@@ -2,19 +2,37 @@ import styled from 'styled-components';
 import { Resizable } from 're-resizable';
 
 export const SGridSidepane = styled(Resizable)`
+  position: relative;
+
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  width: 50px;
 
   background: ${({ theme: { white } }) => white};
   border-right: 3px solid ${({ theme: { light } }) => light};
 
   box-sizing: border-box;
-  transition: background 0.15s ease-in-out 0.1s, border-right 0.1s ease-in-out;
+  transition: background 0.15s ease-in-out 0.1s, border-right 0.1s ease-in-out,
+    max-width 0.2s ease-in-out, min-width 0.2s ease-in-out;
 
   outline: none;
+
+  > .icon {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    height: 2rem;
+    width: 2rem;
+
+    font-size: 1.5rem;
+
+    transition: color 0.15s ease-in-out;
+
+    &:hover {
+      color: ${({ theme: { yellow } }) => yellow};
+    }
+  }
 
   &:hover {
     border-right: 3px solid ${({ theme: { yellow } }) => yellow};
