3d3865397ceac9f3ef4f697098677bfb311e5b0f
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-08-26 15:15:39 +0200

    K-50125 add song lib

---
+++ src/lib/song/song.test.ts
@@ -0,0 +1,26 @@
+import { toSnapshot } from '../util';
+
+import { Song } from '../../store/phraser/types';
+import { withSongChanges } from './index';
+import { toDate } from '../time';
+
+import { getSongMock } from '../../mock/collection';
+
+describe('song', () => {
+  describe('withSongChanges', () => {
+    it('should return editable song with updated change', () => {
+      const editableSong: Song = { ...getSongMock(), changes: [] };
+
+      const updatedEditableSong = withSongChanges('update', 'title', editableSong);
+
+      expect(updatedEditableSong.changes).toEqual([
+        {
+          action: 'update',
+          kind: 'title',
+          date: toDate(Date.now()),
+          snapshot: toSnapshot(editableSong)
+        }
+      ]);
+    });
+  });
+});
