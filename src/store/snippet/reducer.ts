5300ae90c268df96f6adcff71ea7ba5fe7153a93
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-09-10 09:59:21 +0200

    K-41135 dispatch notification when snippet added

--- src/store/snippet/reducer.ts
+++ src/store/snippet/reducer.ts
@@ -3,6 +3,7 @@ import { AddSnippetPayload, DeleteSnippetPayload, Snippet, SnippetsLoadedPayload
 import { useSnippet } from './index';
 import { generateId } from '../../lib/util';
 import { useNotification } from '../notification';
+import { Notification } from '../notification/types';
 
 const { on } = window.electron;
 
@@ -21,10 +22,13 @@ export const snippetsLoadedRecipe = (_: null, { snippets, error }: SnippetsLoade
 
 export const addSnippetRecipe = (_: null, { lines }: AddSnippetPayload) => {
   const { update, snippets } = useSnippet.getState();
+  const { update: notificationUpdate, notifications } = useNotification.getState();
 
   const snippet: Snippet = { id: generateId(), lines };
+  const notification: Notification = { type: 'info', content: 'Snippet added.' };
 
   update({ snippets: [...snippets, snippet] });
+  notificationUpdate({ notifications: [...notifications, notification] });
 };
 
 export const deleteSnippetRecipe = (_: null, { id }: DeleteSnippetPayload) => {
