517a19b2308c100ddff85ffccf000542a07131e3
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-09-13 13:43:34 +0200

    K-41135 extend changes tab test cases

--- src/feature/phraser/changes/changes.test.tsx
+++ src/feature/phraser/changes/changes.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import { usePhraser } from '../../../store/phraser';
@@ -16,6 +16,13 @@ describe('Changes', () => {
     </AppMock>
   );
 
+  test('should render all control buttons', () => {
+    render(ChangesInApp);
+
+    const buttons = screen.getAllByRole('button');
+    expect(buttons.length).toBe(2);
+  });
+
   test('should render all changes when selectedSong is not null', () => {
     const songChangeMock = getSongChangeMock();
     usePhraser.setState(
@@ -26,8 +33,65 @@ describe('Changes', () => {
 
     render(ChangesInApp);
 
+    expect(screen.getByText(songChangeMock.date)).toBeInTheDocument();
+  });
+
+  test('should expand all changes when selectedSong is not null and click on date', () => {
+    const songChangeMock = getSongChangeMock();
+    usePhraser.setState(
+      getPhraserMock({
+        selectedSong: getSongMock({ changes: [songChangeMock] })
+      })
+    );
+
+    render(ChangesInApp);
+
+    act(() => {
+      fireEvent.click(screen.getByText(songChangeMock.date));
+    });
+
     expect(screen.getByText(songChangeMock.kind.toUpperCase())).toBeInTheDocument();
     expect(screen.getByText(songChangeMock.action)).toBeInTheDocument();
+  });
+
+  test('should expand all changes when selectedSong is not null and click on expand button', () => {
+    const songChangeMock = getSongChangeMock();
+    usePhraser.setState(
+      getPhraserMock({
+        selectedSong: getSongMock({ changes: [songChangeMock] })
+      })
+    );
+
+    render(ChangesInApp);
+
+    act(() => {
+      fireEvent.click(screen.getAllByRole('button')[1]);
+    });
+
+    expect(screen.getByText(songChangeMock.kind.toUpperCase())).toBeInTheDocument();
+    expect(screen.getByText(songChangeMock.action)).toBeInTheDocument();
+  });
+
+  test('should collapse all changes when selectedSong is not null and click on collapse button', () => {
+    const songChangeMock = getSongChangeMock();
+    usePhraser.setState(
+      getPhraserMock({
+        selectedSong: getSongMock({ changes: [songChangeMock] })
+      })
+    );
+
+    render(ChangesInApp);
+
+    act(() => {
+      fireEvent.click(screen.getAllByRole('button')[1]);
+    });
+
+    act(() => {
+      fireEvent.click(screen.getAllByRole('button')[0]);
+    });
+
     expect(screen.getByText(songChangeMock.date)).toBeInTheDocument();
+    expect(screen.queryByText(songChangeMock.kind.toUpperCase())).not.toBeInTheDocument();
+    expect(screen.queryByText(songChangeMock.action)).not.toBeInTheDocument();
   });
 });
