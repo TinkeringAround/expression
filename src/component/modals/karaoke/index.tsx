89fddab97575ad5501fefb404cfc567abf961118
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-10-11 10:47:33 +0200

    K-3130 add general modals and phraser karaoke modal

---
+++ src/component/modals/karaoke/index.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useCallback, useEffect, useState } from 'react';
+
+import { usePhraser } from '../../../store/phraser';
+import { setKaraokeMode } from '../../../store/phraser/actions';
+import { flatten } from '../../../lib/util';
+
+import Icon from '../../icon';
+
+import { SKaraoke } from './styled';
+
+export const KARAOKE_MODAL = 'karaoke-modal';
+
+const Karaoke: FC = () => {
+  const { selectedSong } = usePhraser();
+  const [lines, setLines] = useState<string[]>([]);
+
+  const closeKaraokeMode = useCallback(() => {
+    setKaraokeMode(false);
+  }, []);
+
+  useEffect(() => {
+    if (selectedSong) {
+      const combinedLines: string[] = selectedSong.parts.reduce((lines, { rhymes }) => {
+        lines.push(...flatten<string>(rhymes.map(({ lines }) => lines)));
+        return lines;
+      }, [] as string[]);
+      setLines(combinedLines);
+    }
+  }, [selectedSong, setLines]);
+
+  return (
+    <SKaraoke id={KARAOKE_MODAL}>
+      <Icon iconType="cross" onClick={closeKaraokeMode} />
+      {lines.map((line, index) => (
+        <span key={index} className="line">
+          {line}
+        </span>
+      ))}
+    </SKaraoke>
+  );
+};
+
+export default Karaoke;
