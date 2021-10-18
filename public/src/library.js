6af7ca43beb152b7c46331ce59d916ba345f47ce
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-10-18 12:39:05 +0200

    K-4130 added french dictionary

--- public/src/library.js
+++ public/src/library.js
@@ -1,6 +1,7 @@
 const spell = require('nspell');
 const en = require('dictionary-en');
 const de = require('dictionary-de');
+const fr = require('dictionary-fr');
 const { ipcMain } = require('electron');
 
 // ==============================================================
@@ -9,35 +10,48 @@ const { logError, logInfo } = require('./logger');
 const { ACTION } = require(isDev ? '../../src/store/action-types' : './action-types');
 
 // ==============================================================
-const dictionaries = [];
-let library;
+const dictionaries = {
+  en: undefined,
+  de: undefined,
+  fr: undefined
+};
 
 // ==============================================================
 // load dictionaries
-en(registerDictionary);
-de(registerDictionary);
+en((err, dict) => registerDictionary(err, dict, 'en'));
+de((err, dict) => registerDictionary(err, dict, 'de'));
+fr((err, dict) => registerDictionary(err, dict, 'fr'));
 
 // ==============================================================
-function registerDictionary(err, dict) {
+function registerDictionary(err, dict, lang) {
   if (err) {
     throw err;
   }
 
-  dictionaries.push(dict);
+  dictionaries[lang] = dict;
 }
 
 function getSuggestion(event, { word }) {
   try {
     logInfo(`${ACTION.getSuggestion}`);
+    const dictionaryLoaded = Object.values(dictionaries).every(dict => !dict);
+    const suggestions = {
+      en: [],
+      de: [],
+      fr: []
+    };
 
-    // register dictionaries in library
-    if (!library && dictionaries.length > 0) {
-      library = spell(dictionaries);
+    if (dictionaryLoaded) {
+      const errorMsg = `${ACTION.getSuggestion}, raising "Not all Dictionaries registered yet"`;
+      logError(errorMsg);
     }
 
-    const suggestions = spell(dictionaries[1]).suggest(word);
-    logInfo(word);
-    suggestions.forEach(value => logInfo(value));
+    Object.keys(dictionaries).forEach(lang => {
+      if (dictionaries[lang]) {
+        suggestions[lang] = spell(dictionaries[lang]).suggest(word);
+      }
+    });
+
     event.reply(ACTION.addSuggestion, { word, suggestions });
   } catch (error) {
     const errorMsg = `${ACTION.getSuggestion}, raising ${error}`;
